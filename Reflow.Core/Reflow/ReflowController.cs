6b7e1f168466ee844de1bda3af788b0221d9b1cf
Hentov <h_hentov@yahoo.com>
2018-03-11 17:31:54 +0200

    ReflowController cleanup

--- Reflow.Core/Reflow/ReflowController.cs
+++ Reflow.Core/Reflow/ReflowController.cs
@@ -1,19 +1,17 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Logger.Contract;
 using Reflow.Data;
 using Reflow.Data.Contracts;
-using ReflowCore.Controllers;
-using ReflowModels.EntityModels;
-using System;
-using System.IO;
-using Logger;
-using Logger.Contract;
 using ReflowModels.Naming;
+using ReflowModels.EntityModels;
+using ReflowCore.Controllers;
 
 namespace ReflowCore.Reflow
 {
-    #pragma warning disable 1998
+#pragma warning disable 1998
     /// <summary>
     /// Serves as an interface for the front end JS.
     /// </summary>
@@ -33,6 +31,7 @@ namespace ReflowCore.Reflow
             : this(log, new DirectoryStructureController(log), new RenamingController(log))
         {
         }
+
         internal ReflowController(ILog log, params IReflowController[] reflowControllers)
         {
             PokeSingletons();
@@ -45,7 +44,6 @@ namespace ReflowCore.Reflow
         }
 
         
-
         /// <summary>
         /// Returns all tags that the app contains.
         /// </summary>
@@ -54,6 +52,7 @@ namespace ReflowCore.Reflow
         {
             return _renamingController.GetTags();
         }
+
         /// <summary>
         /// Returns all filters for selecting files.        
         /// </summary>
@@ -72,10 +71,12 @@ namespace ReflowCore.Reflow
         {
             return _renamingController.GetFiles(directoryPath.ToString());
         }
+
         public async Task<object> GetDir(object stuff)
         {
             return AppDomain.CurrentDomain.GetData("DataDirectory").ToString();
         }
+        
         /// <summary>
         /// Returns all currently loaded files.
         /// </summary>
@@ -85,17 +86,6 @@ namespace ReflowCore.Reflow
         {
             return _renamingController.GetFileCount();
         }
-        /// <summary>
-        /// Updates filenames (UI only), based on a JSON containing the attributes and their options.
-        /// </summary
-        /// <param name="attributesJson">An array of KVP, containg the name of the attribute and its parameters</param>
-        /// <returns>JSON: Old Filename, NewFilename</returns>
-
-        /// <summary>
-        /// Updates filenames (UI only), based on an JSON, containing the options that the attributes use.
-        /// </summary>
-        /// <param name="newNames">JSON describing the attribute name and its options.</param>
-        /// <returns></returns>
 
         public async Task<object> UpdateTagsStructure(string json)
         {
@@ -106,10 +96,7 @@ namespace ReflowCore.Reflow
         {
             return _renamingController.UpdateTagsData(json);
         }
-        public async Task<object> UpdateFileNames(string newNames)
-        {
-            return null;
-        }
+
         /// <summary>
         /// Sets all global renaming options that the user has chosen.
         /// </summary>
@@ -119,6 +106,7 @@ namespace ReflowCore.Reflow
         {
             return null;
         }
+        
         /// <summary>
         /// Performes the actual on-disk renaming of files, taking into account all user preferences.
         /// This overload is for when all user options are saved into variables on the backend.
@@ -128,11 +116,11 @@ namespace ReflowCore.Reflow
         {
             return _renamingController.RenameFiles();
         }
-       
+
         public void AddTag(string name)
         {
             IUnitOfWork database = new UnitOfWork();
-            database.Tags.Add(new Tag() {Name = name});
+            database.Tags.Add(new Tag() { Name = name });
             database.SaveChanges();
         }
 
@@ -149,8 +137,6 @@ namespace ReflowCore.Reflow
         {
             var a = NameBuilder.Instance.GetType();
         }
-
-        
     }
 }
 #pragma warning restore 1998
